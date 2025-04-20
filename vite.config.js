753d9718b6dfa747713e1acf0ef4e9b96ad30647
Diego <mora8mola@gmail.com>
2025-04-20 18:02:01 +0200

    full

--- vite.config.js
+++ vite.config.js
@@ -12,18 +12,19 @@ export default defineConfig({
     },
   },
   build: {
+    outDir: "docs",
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        about: resolve(__dirname, "quienes-somos.html"),
-        cursos: resolve(__dirname, "cursos.html"),
-        blog: resolve(__dirname, "blog.html"),
-        registro: resolve(__dirname, "registro.html"),
-        login: resolve(__dirname, "login.html"),
-        contacto: resolve(__dirname, "contacto.html"),
+        "quienes-somos": resolve(__dirname, "public/quienes-somos.html"),
+        cursos: resolve(__dirname, "public/cursos.html"),
+        blog: resolve(__dirname, "public/blog.html"),
+        registro: resolve(__dirname, "public/registro.html"),
+        login: resolve(__dirname, "public/login.html"),
+        contacto: resolve(__dirname, "public/contacto.html"),
+        "aviso-legal": resolve(__dirname, "public/aviso-legal.html"),
       },
     },
-    outDir: "docs",
-    emptyOutDir: true,
   },
 });
