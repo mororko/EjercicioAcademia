859adba016a4fb8bd78f4f8f0dbfbdb9f4b84315
Diego <mora8mola@gmail.com>
2025-04-20 20:01:11 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
@@ -15,9 +16,19 @@ export default defineConfig({
     outDir: "docs",
     emptyOutDir: true,
     rollupOptions: {
-      // sólo index
       input: {
         main: resolve(__dirname, "index.html"),
+        "quienes-somos": resolve(__dirname, "quienes-somos.html"),
+        cursos: resolve(__dirname, "cursos.html"),
+        "curso-fullstack1": resolve(__dirname, "curso-fullstack1.html"),
+        "curso-fullstack2": resolve(__dirname, "curso-fullstack2.html"),
+        blog: resolve(__dirname, "blog.html"),
+        noticia1: resolve(__dirname, "noticia1.html"),
+        noticia2: resolve(__dirname, "noticia2.html"),
+        registro: resolve(__dirname, "registro.html"),
+        login: resolve(__dirname, "login.html"),
+        contacto: resolve(__dirname, "contacto.html"),
+        "aviso-legal": resolve(__dirname, "aviso-legal.html"),
       },
     },
   },
